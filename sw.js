6d4a1af03258539e2754e594862889c4a9ef1be5
jerryzuhow77 <jerryzuhow77@gmail.com>
2026-08-20 16:30:57 +0800

    修正收合工具列完整標籤

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '2026-08-20-toolbar-layout-v3';
+const VERSION = '2026-08-20-toolbar-layout-v4';
 const CACHE_PREFIX = 'cpa-alliance-pwa-';
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${VERSION}`;
