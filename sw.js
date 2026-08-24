539320da86a85c187b61def6620dbf8fa3035402
jerryzuhow77 <jerryzuhow77@gmail.com>
2026-08-24 22:09:36 +0800

    Refresh cache for stable mobile map

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '2026-08-24-mobile-history-map-repair-v16';
+const VERSION = '2026-08-24-mobile-history-map-stable-v17';
 const CACHE_PREFIX = 'cpa-alliance-pwa-';
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${VERSION}`;
 const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-${VERSION}`;
