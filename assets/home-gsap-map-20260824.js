fe8626cbd01e2a8fb7b02fbea906844c956afede
jerryzuhow77 <jerryzuhow77@gmail.com>
2026-09-08 20:33:05 +0800

    Refresh loader for vertical activity dialog

--- assets/home-gsap-map-20260824.js
+++ assets/home-gsap-map-20260824.js
@@ -2,7 +2,7 @@
   'use strict';
   var current=document.currentScript&&document.currentScript.src?document.currentScript.src:'';
   var basePath=current?current.slice(0,current.lastIndexOf('/')+1):'./assets/';
-  var version='20260908-activity-horizontal-3';
+  var version='20260908-activity-vertical-dialog-4';
   var balancedArtwork=basePath+'art/east-asia-case-memory-map-paper-clay-20260824.webp?v='+version;
   function sameArtwork(source,target){
     try{return new URL(source,document.baseURI).pathname===new URL(target,document.baseURI).pathname;}
