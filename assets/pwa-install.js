6660bb5a48d1503b0564d530ab25911ff1e0aa91
jerryzuhow77 <jerryzuhow77@gmail.com>
2026-09-13 00:44:14 +0800

    對齊 PWA 註冊與快取命名空間 (#422)
    同步 service worker、PWA 註冊網址與三個首頁載入器的快取版號。

--- assets/pwa-install.js
+++ assets/pwa-install.js
@@ -157,7 +157,7 @@
         reloadingForUpdate = true;
         window.location.reload();
       });
-      navigator.serviceWorker.register(new URL('sw.js?v=20260909-panorama-hd-v12', siteBase).href, {
+      navigator.serviceWorker.register(new URL('sw.js?v=20260912-kaikai-chapter4-v13', siteBase).href, {
         scope: siteBase.pathname,
         updateViaCache: 'none'
       })
